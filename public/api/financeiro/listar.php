0ecaac883175b3d5dca6fbc216aae602920ed00c
Vagner Jorge Souza Feitosa <svagnerjorge@gmail.com>
2025-12-10 12:41:41 -0300

    Update index.html links and clean listar.php headers
    Reorganized index.html sections and updated link labels and URLs for local and global API endpoints. Cleaned up redundant comments and formatting in listar.php for improved readability.

--- public/api/financeiro/listar.php
+++ public/api/financeiro/listar.php
@@ -1,25 +1,22 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 
-// Permite os métodos que você usará (GET, POST, etc.)
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 
-// Permite os cabeçalhos que serão enviados na requisição
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
-// Se for um método OPTIONS (pré-voo CORS), encerra a execução
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit(0);
 }
 
-header("Content-Type: application/json"); // define retorno JSON
+header("Content-Type: application/json"); 
 require_once "../conexao.php";
 
 try {
     $stmt = $pdo->query("SELECT * FROM financeiro");
     $carros = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    echo json_encode($carros); // envia array JSON
+    echo json_encode($carros);
 } catch (PDOException $e) {
     echo json_encode(["erro" => $e->getMessage()]);
 }
